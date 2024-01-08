f16493fddfb8b772f24ec6327ca925a614e5b58f
syorito-hatsuki <enderdron3@gmail.com>
2024-01-08 18:34:04 +0200

    Completed task 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {useEffect, useState} from "react";
+
+interface HeartRate {
+    lower: string,
+    upper: string
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [ageString, setAgeString] = useState("")
+    const [heartRate, setHeartRate] = useState<HeartRate>({
+        lower: "",
+        upper: ""
+    })
+
+    useEffect(() => {
+        const age = Number(ageString)
+        return age > 0 ? setHeartRate({
+            lower: ((220 - age) * 0.65).toFixed(),
+            upper: ((220 - age) * 0.85).toFixed()
+        }) : setAgeString("");
+    }, [ageString])
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.input}
+                       placeholder="Enter your age"
+                       defaultValue="0"
+                       value={ageString}
+                       onChangeText={text => setAgeString(text)}
+                       keyboardType="numeric"/>
+            {ageString !== "" && <Text>{heartRate.lower}-{heartRate.upper}</Text>}
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8
+    },
+    input: {
+        borderColor: "gray",
+        width: "50%",
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+    },
 });
